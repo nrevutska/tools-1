298921c0147670615a25a36b441b2950f597d867
Nataliya Revutska <nrevu@softserveinc.com>
2020-10-19 21:38:40 +0300

    concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 const {dest, src} = require('gulp');
 const gulp = require('gulp');
+const concat = require('gulp-concat')
 
 const task1 = async () => {
     return src("./**/*.js")
-        .pipe(dest("./output/result.js"));
+        .pipe(concat("./output/result.js"));
 }
 
 const task2 = async () => {
     return src("./**/*.css")
-        .pipe(dest("./output/result.css"));
+        .pipe(concat("./output/result.css"));
 }
 
 const groupTask = gulp.parallel(task1, task2);
