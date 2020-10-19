8fce8b5be8baa4383cb1e8b49562d8f4ac6f5896
Nataliya Revutska <nrevu@softserveinc.com>
2020-10-19 21:24:24 +0300

    created gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,15 @@
+const {dest, src} = require('gulp');
 
+const task1 = async () => {
+    return src("./**/*.js")
+        .pipe(dest("./output/result.js"));
+}
+
+const task2 = async () => {
+    return src("./**/*.css")
+        .pipe(dest("./output/result.css"));
+}
+
+const groupTask = gulp.parallel(task1, task2);
+
+exports.default = groupTask;
